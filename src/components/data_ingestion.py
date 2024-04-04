40e2ec4ef2b50d5cc3928b947791df97757c2c71
panshuljindal <panshuljindal@gmail.com>
2024-04-04 18:55:43 -0500

    Data ingestion completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    trainDataPath: str = os.path.join('artifacts', 'train.csv')
+    testDataPath: str = os.path.join('artifacts', 'test.csv')
+    rawDataPath: str = os.path.join('artifacts', 'raw.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestionConfig = DataIngestionConfig()
+    
+    def initiateDataIngestion(self):
+        logging.info("Enter Data Ingestion Method")
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info("Read dataset as dataframe")
+
+            os.mkdir(os.path.dirname(self.ingestionConfig.rawDataPath)) if not os.path.exists(os.path.dirname(self.ingestionConfig.rawDataPath)) else None
+            
+            df.to_csv(self.ingestionConfig.rawDataPath, index=False)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestionConfig.trainDataPath, index=False)
+            test_set.to_csv(self.ingestionConfig.testDataPath, index=False)
+
+            logging.info("Data Ingestion Completed")
+
+            return (
+                self.ingestionConfig.trainDataPath,
+                self.ingestionConfig.testDataPath
+            )
+
+        except Exception as e:
+            logging.info("Error in Data Ingestion Method")
+            raise CustomException(e,sys)
+
+if __name__ == "__main__":
+    dataIngestion = DataIngestion()
+    dataIngestion.initiateDataIngestion()
